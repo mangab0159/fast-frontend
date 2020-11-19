1152e6622e3d174dd44e6402fabfc7f460d43df0
허만갑 <aksrkq93@naver.com>
2020-11-19 13:44:29 +0900

    20201119 1344 Prevent a directory traversal attack

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
 
 Vue.use(VueRouter);
 
@@ -11,28 +12,34 @@ const routes = [
   {
     path: '/patients',
     component: () => import('@/views/PatientsPage.vue'),
+    meta: { auth: true },
   },
   {
     path: '/patients/:ptname',
     component: () => import('@/views/PatientsPage.vue'),
+    meta: { auth: true },
   },
   {
     path: '/pcontents/:ptid',
     name: 'pcontent',
     component: () => import('@/views/PcontentsPage.vue'),
+    meta: { auth: true },
   },
   {
     path: '/tresult',
     name: 'tresult',
     component: () => import('@/views/TresultPage.vue'),
+    meta: { auth: true },
   },
   {
     path: '/worklists',
     component: () => import('@/views/WorklistsPage.vue'),
+    meta: { auth: true },
   },
   {
     path: '*',
     component: () => import('@/views/NotFoundPage.vue'),
+    meta: { auth: true },
   },
 ];
 
@@ -40,6 +47,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    console.log('인증필요');
+    next('/');
+    return;
+  }
+  console.log('next', next);
+  next();
 });
 
 export default router;
